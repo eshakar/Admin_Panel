aa93a79229dba53f4b0febff7b10f44f1713fb60
eshakar <102459513+eshakar@users.noreply.github.com>
2024-11-26 22:32:56 +0530

    Update breakpoints.js

--- src/assets/theme/base/breakpoints.js
+++ src/assets/theme/base/breakpoints.js
@@ -1,23 +1,4 @@
-/**
-=========================================================
-* Admin Dashboard React - v3.1.0
-=========================================================
 
-* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard-pro-react
-* Copyright 2023 Esha (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-/**
- * The base breakpoints for the Admin Dashboard React.
- * You can add new breakpoints using this file.
- * You can customized the breakpoints for the entire Admin Dashboard React using thie file.
- */
 
 const breakpoints = {
   values: {
